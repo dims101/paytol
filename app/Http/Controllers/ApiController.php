247695ade43e6a0560b7d7416fbf615a375b955d
dims101 <dimas.stmp@gmail.com>
2022-07-21 20:01:06 +0700

    error ga bisa scan golongan error

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -139,9 +139,11 @@ class ApiController extends Controller
                     ->first();
         $golongan = Vehicle::where('nama',$user->kendaraan)
                     ->pluck('golongan');
+        return $golongan;die;
         $golongan = str_replace('"', "", $golongan);
         $golongan = str_replace('[', "", $golongan);
         $golongan = str_replace(']', "", $golongan);
+
         $rute = Route::select('id','tarif_golongan_'.$golongan)
                     ->where('id_gate_masuk',$request->id_gate_masuk)
                     ->where('id_gate_keluar',$request->id_gate_keluar)
